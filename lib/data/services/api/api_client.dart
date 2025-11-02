3a430009891274e26f190c760362755a14b0bf3b
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2025-11-02 20:48:46 +0630

    Add: Dio

---
+++ lib/data/services/api/api_client.dart
@@ -0,0 +1,49 @@
+import 'package:dio/dio.dart';
+
+class ApiClient {
+  final String baseUrl;
+  final String? accessToken;
+
+  late Dio _dio;
+
+  ApiClient({this.accessToken, required this.baseUrl}) {
+    final BaseOptions dioOptions = BaseOptions(
+      baseUrl: baseUrl,
+      headers: accessToken != null
+          ? {"Authorization": 'Bearer $accessToken'}
+          : null,
+    );
+
+    _dio = Dio(dioOptions);
+    _dio.interceptors.add(_errorInterceptor());
+    _dio.interceptors.add(_successInterceptor());
+  }
+
+  Interceptor _errorInterceptor() {
+    return InterceptorsWrapper(
+      onRequest: (RequestOptions options, RequestInterceptorHandler handler) {
+        return handler.next(options);
+      },
+      onResponse: (Response response, ResponseInterceptorHandler handler) {
+        return handler.next(response);
+      },
+      onError: (DioException error, ErrorInterceptorHandler handler) {
+        return handler.next(error);
+      },
+    );
+  }
+
+  Interceptor _successInterceptor() {
+    return InterceptorsWrapper(
+      onRequest: (options, handler) {
+        return handler.next(options);
+      },
+      onResponse: (response, handler) {
+        return handler.next(response);
+      },
+      onError: (error, handler) {
+        return handler.next(error);
+      },
+    );
+  }
+}
